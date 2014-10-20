cc24b05909dfd46dac9c6c0614a3494716d20853
Baiju Muthukadan <baiju.m.mail@gmail.com>
2014-10-20 12:43:34 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Golang utilities for Google+ Sign-In (server-side apps)
+=======================================================
+
+Code derived from https://github.com/googleplus/gplus-quickstart-go
---
+++ plus.go
@@ -0,0 +1,55 @@
+package plus
+
+import (
+	"encoding/base64"
+	"fmt"
+	"encoding/json"
+	"strings"
+)
+
+// ClaimSet represents an IdToken response.
+type ClaimSet struct {
+	Sub string
+}
+
+
+// decodeIdToken takes an ID Token and decodes it to fetch the Google+ ID within
+func DecodeIdToken(idToken string) (gplusID string, err error) {
+	// An ID token is a cryptographically-signed JSON object encoded in base 64.
+	// Normally, it is critical that you validate an ID token before you use it,
+	// but since you are communicating directly with Google over an
+	// intermediary-free HTTPS channel and using your Client Secret to
+	// authenticate yourself to Google, you can be confident that the token you
+	// receive really comes from Google and is valid. If your server passes the ID
+	// token to other components of your app, it is extremely important that the
+	// other components validate the token before using it.
+	var set ClaimSet
+	if idToken != "" {
+		// Check that the padding is correct for a base64decode
+		parts := strings.Split(idToken, ".")
+		if len(parts) < 2 {
+			return "", fmt.Errorf("Malformed ID token")
+		}
+		// Decode the ID token
+		b, err := base64Decode(parts[1])
+		if err != nil {
+			return "", fmt.Errorf("Malformed ID token: %v", err)
+		}
+		err = json.Unmarshal(b, &set)
+		if err != nil {
+			return "", fmt.Errorf("Malformed ID token: %v", err)
+		}
+	}
+	return set.Sub, nil
+}
+
+func base64Decode(s string) ([]byte, error) {
+	// add back missing padding
+	switch len(s) % 4 {
+	case 2:
+		s += "=="
+	case 3:
+		s += "="
+	}
+	return base64.URLEncoding.DecodeString(s)
+}

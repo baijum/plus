ce0fed66b875174fcddffdccf29da45a974e9862
Baiju Muthukadan <baiju.m.mail@gmail.com>
2014-10-20 13:02:31 +0530

    move docs to main file

--- plus.go
+++ plus.go
@@ -1,3 +1,12 @@
+// Package plus provides utilities for Google+ Sign-In (server-side apps)
+//
+// Example:
+//
+//   gplusID, err := plus.DecodeIdToken(idToken)
+//   if err != nil {
+//   	log.Fatal("Error decoding ID token: ", err)
+//   }
+
 package plus
 
 import (

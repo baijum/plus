35ffe6d6e21a96b13ba4b2aa11654a62704e8b27
Baiju Muthukadan <baiju.m.mail@gmail.com>
2014-10-20 13:07:44 +0530

    update docs to reflect API change

--- plus.go
+++ plus.go
@@ -2,7 +2,7 @@
 //
 // Example:
 //
-//   gplusID, err := plus.DecodeIdToken(idToken)
+//   gplusID, err := plus.DecodeIDToken(idToken)
 //   if err != nil {
 //   	log.Fatal("Error decoding ID token: ", err)
 //   }
@@ -21,7 +21,7 @@ type ClaimSet struct {
 	Sub string
 }
 
-// DecodeIdToken takes an ID Token and decodes it to fetch the Google+ ID within
+// DecodeIDToken takes an ID Token and decodes it to fetch the Google+ ID within
 func DecodeIDToken(idToken string) (gplusID string, err error) {
 	// An ID token is a cryptographically-signed JSON object encoded in base 64.
 	// Normally, it is critical that you validate an ID token before you use it,
